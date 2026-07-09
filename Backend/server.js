3bfe2ecfe16edac3852ba992912c7030dc474f06
advitya <advitya9@gmail.com>
2026-07-09 14:44:19 +0530

    optimizations

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,14 @@ import inngestRoutes from "./Routes/inngestRoutes.js";
 import soloCheckInRoutes from "./Routes/soloCheckInRoutes.js";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(clerkMiddleware());
 app.use("/api/users",userRoutes);
@@ -29,9 +36,11 @@ app.get("/",(req,res)=>{
 app.use("/api/solo-checkins", soloCheckInRoutes);
 const server=http.createServer(app);
 initSocket(server);
-connectDB().then(
-    server.listen(process.env.PORT,()=>{
-    console.log("Server running");
-})
-);
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
 
